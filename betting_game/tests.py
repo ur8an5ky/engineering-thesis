818c29f0e9236495c90e99c69dd7d49f5f20e388
Kuba <urbanski.jakub77@gmail.com>
2023-09-21 11:42:11 +0200

    test: add tests for all necessary views and functions

--- betting_game/tests.py
+++ betting_game/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import FootballTeams, FootballMatches
 
-# Create your tests here.
+class TestFootballMatch(TestCase):
+    def setUp(self):
+        # Tworzymy dwie drużyny do testów
+        self.team1 = FootballTeams.objects.create(name="Team1", country="Country1", city="City1", group="A")
+        self.team2 = FootballTeams.objects.create(name="Team2", country="Country2", city="City2", group="B")
+
+    def test_add_match(self):
+        # Utworzenie nowego meczu
+        match = FootballMatches.objects.create(
+            id_hosts=self.team1,
+            score_hosts=0,
+            id_visitors=self.team2,
+            score_visitors=0,
+            phase="Phase",
+            start=timezone.now(),
+            isPenalties=False
+        )
+
+        # Sprawdzenie, czy mecz został dodany poprawnie
+        self.assertEqual(FootballMatches.objects.count(), 1)
+        self.assertEqual(match.id_hosts, self.team1)
+        self.assertEqual(match.score_hosts, 0)
+        self.assertEqual(match.id_visitors, self.team2)
+        self.assertEqual(match.score_visitors, 0)
+        self.assertEqual(match.phase, "Phase")
+        self.assertIsNotNone(match.start)
+        self.assertFalse(match.isPenalties)
+        self.assertEqual(str(match), f"{self.team1} vs {self.team2} - {match.start}")
+
+    def test_edit_match_score(self):
+        # Utworzenie nowego meczu
+        match = FootballMatches.objects.create(
+            id_hosts=self.team1,
+            score_hosts=0,
+            id_visitors=self.team2,
+            score_visitors=0,
+            phase="Phase",
+            start=timezone.now(),
+            isPenalties=False
+        )
+
+        # Edycja wyniku meczu
+        match.score_hosts = 3
+        match.score_visitors = 2
+        match.save()
+
+        # Pobranie z bazy zaktualizowanego meczu
+        updated_match = FootballMatches.objects.get(pk=match.pk)
+
+        # Sprawdzenie, czy wynik został zaktualizowany poprawnie
+        self.assertEqual(updated_match.score_hosts, 3)
+        self.assertEqual(updated_match.score_visitors, 2)
+        self.assertEqual(str(updated_match), f"{self.team1} vs {self.team2} - {updated_match.start}")
--- betting_game_api/tests.py
+++ betting_game_api/tests.py
@@ -1,3 +1,90 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from betting_game.models import FootballTeams, FootballMatches
 
-# Create your tests here.
+class FootballTeamsDetailTests(TestCase):
+    def setUp(self):
+        self.team = FootballTeams.objects.create(
+            name='Example Team',
+            country='Example',
+            city='Example City',
+            group='A'
+        )
+        self.match = FootballMatches.objects.create(
+            id_hosts=self.team,
+            score_hosts=0,
+            id_visitors=self.team,
+            score_visitors=0,
+            phase='Phase',
+            start='2023-09-21T12:00Z',
+            isPenalties=False
+        )
+
+    def test_retrieve_team_and_matches(self):
+        url = reverse('betting_game_api:team', kwargs={'pk': self.team.id_team})
+        client = APIClient()
+        response = client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        team = response.data['team']
+        matches = response.data['matches']
+
+        self.assertEqual(team['name'], 'Example Team')
+        self.assertEqual(len(matches), 1)
+        self.assertEqual(matches[0]['id_match'], self.match.id_match)
+
+
+class GroupTests(TestCase):
+    def setUp(self):
+        self.team = FootballTeams.objects.create(
+            name='Example Team',
+            country='Example',
+            city='Example City',
+            group='A'
+        )
+        self.match = FootballMatches.objects.create(
+            id_hosts=self.team,
+            score_hosts=0,
+            id_visitors=self.team,
+            score_visitors=0,
+            phase='group',
+            start='2023-09-21T12:00Z',
+            isPenalties=False
+        )
+
+    def test_group_view(self):
+        url = reverse('betting_game_api:group', kwargs={'group': self.team.group.lower()})
+        client = APIClient()
+        response = client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+        teams = response.data['teams']
+        matches = response.data['matches']
+
+        self.assertEqual(len(teams), 1)
+        self.assertEqual(len(matches), 1)
+
+class CountryTests(TestCase):
+    def setUp(self):
+        self.team = FootballTeams.objects.create(
+            name='Example Team',
+            country='Example',
+            city='Example City',
+            group='A'
+        )
+
+    def test_country_view(self):
+        url = reverse('betting_game_api:country', kwargs={'country': self.team.country.lower()})
+        client = APIClient()
+        response = client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+        teams = response.data
+
+        self.assertEqual(len(teams), 1)
+        self.assertEqual(teams[0]['name'], 'Example Team')
