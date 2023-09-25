763b1c53dfcd58333c837a8603206113a527e5b1
Kuba <urbanski.jakub77@gmail.com>
2023-09-25 20:17:17 +0200

    fix: make minor backend improvements

--- engineering-thesis-django/betting_game/tests.py
+++ engineering-thesis-django/betting_game/tests.py
@@ -4,12 +4,10 @@ from .models import FootballTeams, FootballMatches
 
 class TestFootballMatch(TestCase):
     def setUp(self):
-        # Tworzymy dwie drużyny do testów
         self.team1 = FootballTeams.objects.create(name="Team1", country="Country1", city="City1", group="A")
         self.team2 = FootballTeams.objects.create(name="Team2", country="Country2", city="City2", group="B")
 
     def test_add_match(self):
-        # Utworzenie nowego meczu
         match = FootballMatches.objects.create(
             id_hosts=self.team1,
             score_hosts=0,
@@ -20,7 +18,6 @@ class TestFootballMatch(TestCase):
             isPenalties=False
         )
 
-        # Sprawdzenie, czy mecz został dodany poprawnie
         self.assertEqual(FootballMatches.objects.count(), 1)
         self.assertEqual(match.id_hosts, self.team1)
         self.assertEqual(match.score_hosts, 0)
@@ -32,7 +29,6 @@ class TestFootballMatch(TestCase):
         self.assertEqual(str(match), f"{self.team1} vs {self.team2} - {match.start}")
 
     def test_edit_match_score(self):
-        # Utworzenie nowego meczu
         match = FootballMatches.objects.create(
             id_hosts=self.team1,
             score_hosts=0,
@@ -43,15 +39,12 @@ class TestFootballMatch(TestCase):
             isPenalties=False
         )
 
-        # Edycja wyniku meczu
         match.score_hosts = 3
         match.score_visitors = 2
         match.save()
 
-        # Pobranie z bazy zaktualizowanego meczu
         updated_match = FootballMatches.objects.get(pk=match.pk)
 
-        # Sprawdzenie, czy wynik został zaktualizowany poprawnie
         self.assertEqual(updated_match.score_hosts, 3)
         self.assertEqual(updated_match.score_visitors, 2)
         self.assertEqual(str(updated_match), f"{self.team1} vs {self.team2} - {updated_match.start}")
--- engineering-thesis-django/core/settings.py
+++ engineering-thesis-django/core/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'betting_game',
     'betting_game_api',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -131,3 +133,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
