24c912143950b391dd9c73274a5980c3ca250902
Kuba <urbanski.jakub77@gmail.com>
2023-10-02 19:44:07 +0200

    feat(backend): add custom user model and new user registration with JWT

--- engineering-thesis-django/core/urls.py
+++ engineering-thesis-django/core/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('betting_game.urls', namespace='betting_game')),
     path('api/', include('betting_game_api.urls', namespace='betting_game_api')),
+    path('api/user/', include('users.urls', namespace='users')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
